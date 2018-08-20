8a13c94d4023f9c28e4e21d77a2b111ee3dd0250
abhirocks550 <abhirocks550@gmail.com>
2018-08-20 21:49:10 +0530

    Add babel to project

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /**
  * index.js
  */
-console.log('Hello, world!');
+const logSomething = options => ({
+  ...options,
+  age: 26
+});
+
+const options = logSomething({one: '1', two: '2'});
+
+console.log(options);
