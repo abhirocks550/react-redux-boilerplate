67924e3900cdf2a0f89df147481ce817770d54e5
abhirocks550 <abhirocks550@gmail.com>
2018-08-21 00:04:40 +0530

    Add express server

---
+++ app.js
@@ -0,0 +1,50 @@
+/**
+ * app.js
+ */
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import routes from './routes';
+
+// Express app setup
+const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+
+// logger
+app.use(logger('combined'));
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// cookie parser
+app.use(cookieParser());
+
+// use routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {},
+  });
+  next();
+});
+
+export default app;
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 /**
  * index.js
  */
-const logSomething = options => ({
-  ...options,
-  age: 26,
-});
 
-const options = logSomething({ one: '1', two: '2' });
-
-console.log(options);
+require('babel-register');
+require('./server/index');
